755206f3a7af7f803b83982eead41e6370d7d4d2
Il-Youp Kwak <ikwak2@cau.ac.kr>
2021-06-28 16:10:02 +0000

    version 1.50

--- R/aSPUsPath2.r
+++ R/aSPUsPath2.r
@@ -35,12 +35,12 @@
 #' data(kegg9)
 #'
 #' # p-values of SPUpath and aSPUpath tests.
-#' out.a <- aSPUsPath2(kegg9$nP, corSNP = kegg9$ldmatrix, pow=c(1:8, Inf),
-#'                   pow2 = c(1,2,4,8), 
-#'                   snp.info=kegg9$snp.info, gene.info = kegg9$gene.info,
-#'                   n.perm=10, Ps = TRUE)
+#' \dontrun{out.a <- aSPUsPath2(kegg9$nP, corSNP = kegg9$ldmatrix, pow=c(1:8, Inf),
+#'                  pow2 = c(1,2,4,8), 
+#'                  snp.info=kegg9$snp.info, gene.info = kegg9$gene.info,
+#'                  n.perm=1000, Ps = TRUE) }
 #'
-#' out.a
+#' #out.a
 #'
 #' @seealso \code{\link{aSPUs}}
 #'
